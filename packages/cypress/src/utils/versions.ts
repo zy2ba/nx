cc9b2eddc51aae2d8370f53010692ec58023bb8e
Caleb Ukle <caleb@nrwl.io>
2022-04-06 11:08:33 -0500

    cleanup(testing): bump cypress version
    bump to latest cypress v10 release

--- packages/cypress/src/utils/versions.ts
+++ packages/cypress/src/utils/versions.ts
@@ -5,7 +5,7 @@ export const typesNodeVersion = '16.11.7';
  * TODO(caleb): this is only the mac version, and temp until there is a v10 release
  */
 export const cypressVersion =
-  'https://cdn.cypress.io/beta/npm/10.0.0/darwin-x64/10.0-release-8765e6481bbc3b8247bd6f64139b8d3676d84c89/cypress.tgz';
+  'https://cdn.cypress.io/beta/npm/10.0.0/darwin-x64/10.0-release-c8c2c7de443fd41cadf30d1ea2ec1e17bbf6366c/cypress.tgz';
 export const cypressReactVersion = '^5.12.1';
 export const cypressWebpackVersion = '^1.8.0';
 export const webpackHttpPluginVersion = '^5.5.0';
